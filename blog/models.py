131aed3841253b3b55d16e9b233e8281b5bc95b0
ADARSHTHOMAS <adarshthomasv@gmail.com>
2021-02-08 00:55:07 +0530

    made changes for Comment Model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.text
+
